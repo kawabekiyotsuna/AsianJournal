eb0463219ae4e5d32dc92bc501916ac4403ee080
EC2 Default User <ec2-user@ip-172-31-27-204.ap-northeast-1.compute.internal>
2020-04-29 02:07:47 +0000

    asset関数をhttpsにリダイレクト

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (request()->isSecure()) {
+            \URL::forceScheme('https');
+        }
+        
         Schema::defaultStringLength(191);
     }
 }
