701810124cae4c36b5545424bd142aa283b9ea8a
EC2 Default User <ec2-user@ip-172-31-27-204.ap-northeast-1.compute.internal>
2020-04-29 03:48:51 +0000

    フッターを変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -91,7 +91,7 @@
             @yield('content')
         </main>
         <footer class='footer p20'>
-          <small class='copyright'>Laravel Book Reviews 2019 copyright</small>
+          <small class='copyright'>AsianJournal 2020 copyright</small>
         </footer>
     </div>
 </body>
